e3f3e3141de3ed72a3aa6e6e2d82b3a44fb39d02
grizzlyxdev <t22qd95x26@privaterelay.appleid.com>
2026-05-19 07:53:34 +0700

    Add simple hello world script

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,1 @@
-import argparse
-
-def greet(name: str, times: int):
-    for _ in range(times):
-        print(f"Hello, {name}!")
-
-def main():
-    parser = argparse.ArgumentParser(description="Print a greeting multiple times.")
-    parser.add_argument("--name", type=str, default="World", help="Name to greet")
-    parser.add_argument("--times", type=int, default=1, help="Number of times to greet")
-    args = parser.parse_args()
-    greet(args.name, args.times)
-
-if __name__ == "__main__":
-    main()
+print("Hello, World!")
