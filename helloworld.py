1016f1f45d8971996d536858fd4b20481aebb1b8
grizzlyxdev <t22qd95x26@privaterelay.appleid.com>
2026-05-19 07:44:42 +0700

    Add simple hello world script

--- helloworld.py
+++ helloworld.py
@@ -1,22 +1,5 @@
-import sys
-import argparse
-
-def greet(name: str, upper: bool = False) -> str:
-    """Return a greeting string.
-    If `upper` is True, the greeting is returned in uppercase.
-    """
-    greeting = f"Hello, {name}!"
-    return greeting.upper() if upper else greeting
-
 def main():
-    parser = argparse.ArgumentParser(description="Print a greeting.")
-    parser.add_argument("name", nargs="*", default=["World"], help="Name(s) to greet")
-    parser.add_argument("-r", "--repeat", type=int, default=1, help="Number of times to repeat the greeting")
-    parser.add_argument("-u", "--upper", action="store_true", help="Convert greeting to uppercase")
-    args = parser.parse_args()
-    name = " ".join(args.name)
-    for _ in range(args.repeat):
-        print(greet(name, args.upper))
+    print("Hello, World!")
 
 if __name__ == "__main__":
     main()
