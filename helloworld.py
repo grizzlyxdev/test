e36265d8cfd7e83c783e137e4aed71df4b13e8c9
grizzlyxdev <t22qd95x26@privaterelay.appleid.com>
2026-05-19 07:50:04 +0700

    Enhance helloworld script with argparse support

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,15 @@
+import argparse
+
+def greet(name: str, times: int):
+    for _ in range(times):
+        print(f"Hello, {name}!")
+
 def main():
-    print("Hello, World!")
+    parser = argparse.ArgumentParser(description="Print a greeting multiple times.")
+    parser.add_argument("--name", type=str, default="World", help="Name to greet")
+    parser.add_argument("--times", type=int, default=1, help="Number of times to greet")
+    args = parser.parse_args()
+    greet(args.name, args.times)
 
 if __name__ == "__main__":
     main()
